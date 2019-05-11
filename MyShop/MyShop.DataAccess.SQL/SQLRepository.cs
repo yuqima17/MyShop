ef99016274d827b485714f6ee8f035d16344e460
yuqima17 <yuqi_m@yahoo.com>
2019-05-11 13:13:59 -0400

    created SQL repo to replace inmemory repo

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,57 @@
+﻿using MyShop.Core.Contract;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbset;
+        public SQLRepository(DataContext context1)
+        {
+            this.context = context1;
+            this.dbset = context.Set<T>();
+        }
+        public IQueryable<T> Collections()
+        {
+            return dbset;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = Find(id);
+            if (context.Entry(t).State == EntityState.Detached)
+            {
+                dbset.Attach(t);
+            }
+            dbset.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbset.Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            dbset.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbset.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
