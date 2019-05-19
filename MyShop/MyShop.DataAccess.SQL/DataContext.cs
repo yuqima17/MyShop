3b51f50d5be3a18974cd23b348c9c2e7b7368552
yuqima17 <yuqi_m@yahoo.com>
2019-05-18 21:35:12 -0400

    Creating Basket Model

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -17,5 +17,7 @@ namespace MyShop.DataAccess.SQL
         }
         public DbSet<Product> products { get; set; }
         public DbSet<ProductCategory> productCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
