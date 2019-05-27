8e70a28be86197965499401e480bb31fd306b3b6
yuqima17 <yuqi_m@yahoo.com>
2019-05-27 15:09:29 -0400

    Created Customer model

--- MyShop/MyShop.WebUI/Controllers/AccountController.cs
+++ MyShop/MyShop.WebUI/Controllers/AccountController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using MyShop.Core.Contract;
+using MyShop.Core.Models;
 using MyShop.WebUI.Models;
 
 namespace MyShop.WebUI.Controllers
@@ -17,15 +19,13 @@ namespace MyShop.WebUI.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        private IRepository<Customer> customerRepository;
 
-        public AccountController()
-        {
-        }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(IRepository<Customer> customerRepository )
         {
-            UserManager = userManager;
-            SignInManager = signInManager;
+
+            this.customerRepository = customerRepository;
         }
 
         public ApplicationSignInManager SignInManager
@@ -155,6 +155,19 @@ namespace MyShop.WebUI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //register the customer model
+                    Customer customer = new Customer() {
+                        City =model.City,
+                        Street=model.Street,
+                        State=model.State,
+                        ZipCode=model.ZipCode,
+                        Email =model.Email,
+                        FirstName =model.FirstName,
+                        LastName=model.LastName,
+                        UserId=user.Id
+                    };
+                    customerRepository.Insert(customer);
+                    customerRepository.Commit();
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
--- MyShop/MyShop.WebUI/Controllers/ManageController.cs
+++ MyShop/MyShop.WebUI/Controllers/ManageController.cs
@@ -20,11 +20,7 @@ namespace MyShop.WebUI.Controllers
         {
         }
 
-        public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
-        {
-            UserManager = userManager;
-            SignInManager = signInManager;
-        }
+
 
         public ApplicationSignInManager SignInManager
         {
